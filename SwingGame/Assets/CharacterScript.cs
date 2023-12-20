e87d5f971274d32a602face763de1fbf5c18081c
Christopher Chau <chrischau04@gmail.com>
2023-12-20 14:07:54 -0800

    character jumping is good now

--- SwingGame/Assets/CharacterScript.cs
+++ SwingGame/Assets/CharacterScript.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
 {
-    public Rigidbody2D myRigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // Check for jump input
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown("space"))
         {
-            myRigidbody.velocity = Vector2.up * 10;
+            GetComponent<Rigidbody2D>().velocity = Vector2.up * 14;
         }
         else
         {
@@ -28,10 +27,12 @@ public class CharacterScript : MonoBehaviour
             // Calculate the movement direction
             Vector2 movement = new Vector2(horizontalInput, verticalInput).normalized;
 
-            // Set the velocity based on the calculated movement direction
-            myRigidbody.velocity = new Vector2(movement.x * 5, movement.y * 5);
+            // Only set velocity if there is non-zero input
+            if (movement.magnitude > 0)
+            {
+                // Set the velocity based on the calculated movement direction
+                GetComponent<Rigidbody2D>().velocity = new Vector2(movement.x * 5, movement.y * 5);
+            }
         }
     }
-
-
 }
